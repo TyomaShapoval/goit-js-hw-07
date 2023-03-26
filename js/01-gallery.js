b92e20153dda6bff319c29b8d6f4176495bd1b0e
TyomaShapoval <shapoval1044@gmail.com>
2023-03-26 22:18:42 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryUl = document.querySelector(".gallery")
 
-console.log(galleryItems);
+const imagesLi = galleryItems.map(({preview, original, description}) => {
+   const list = document.createElement("li");
+   list.classList.add("gallery__item");
+   const refer = document.createElement("a");
+   refer.classList.add("gallery__link");
+   list.append(refer);
+   const image = document.createElement("img");
+   image.classList.add("gallery__image")
+   image.setAttribute("src", preview)
+   image.setAttribute("alt", description)
+   image.onclick
+
+   image.onclick = () => {
+    basicLightbox.create(`
+		<img width="1400" height="900" src="${original}">
+	`).show()}
+
+   refer.append(image)
+   
+   return list
+})
+galleryUl.append(...imagesLi)
+
