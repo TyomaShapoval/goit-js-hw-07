43a2ecd3897077026a22d9eaeba81c8e67f00976
TyomaShapoval <shapoval1044@gmail.com>
2023-03-30 21:41:26 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,17 +7,28 @@ const imagesLi = galleryItems.map(({preview, original, description}) => {
    list.classList.add("gallery__item");
    const refer = document.createElement("a");
    refer.classList.add("gallery__link");
+   refer.href = original;
+   refer.addEventListener(`click`, event => event.preventDefault())
    list.append(refer);
    const image = document.createElement("img");
    image.classList.add("gallery__image")
    image.setAttribute("src", preview)
    image.setAttribute("alt", description)
-   image.onclick
+   image.setAttribute("data-source", original)
 
    image.onclick = (() => {
    const modal = basicLightbox.create(`
 		<img width="1400" height="900" src="${original}">
-	`).show();
+	`);
+   modal.show()
+   const handleKeyPress = (event) => {
+      if (event.key === 'Escape') {
+        modal.close();
+        galleryUl.removeEventListener('keydown', handleKeyPress);
+      }
+    };
+  
+    galleryUl.addEventListener('keydown', handleKeyPress);
    
 })
    
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryUl = document.querySelector(".gallery")
+
+const imagesLi = galleryItems.map(({preview, original, description}) => {
+   const list = document.createElement("li");
+   list.classList.add("gallery__item");
+   const refer = document.createElement("a");
+   refer.classList.add("gallery__link");
+   refer.href = original;
+   refer.addEventListener(`click`, event => event.preventDefault())
+   list.append(refer);
+   const image = document.createElement("img");
+   image.classList.add("gallery__image");
+   image.classList.add("lightbox");
+   image.setAttribute("src", preview)
+   image.setAttribute("alt", description)
+
+   refer.append(image)
+   
+   return list
+})
+galleryUl.append(...imagesLi);
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
